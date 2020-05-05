a7962161f64276e1df47414ebc447f9ff9742886
Alex Frost <zufrost.a@gmail.com>
2020-05-05 23:35:56 +0300

    stream initialize

---
+++ scr/com/zufrost/learn/StreamLearnMain.java
@@ -0,0 +1,18 @@
+package com.zufrost.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamLearnMain {
+    public static void main(String[] args) {
+        List<String> items = new ArrayList<String>();
+
+        items.add("one");
+        items.add("two");
+        items.add("three");
+
+        Stream<String> stream = items.stream();
+        System.out.println(stream);
+    }
+}
